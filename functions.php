8a92c06c4a48f8796b70e42fc3c2751311e22499
Nelson Izquierdo <izquierdonelson@gmail.com>
2018-01-11 02:43:26 -0300

    Fontastic Local

--- functions.php
+++ functions.php
@@ -46,9 +46,3 @@ function wpdocs_custom_excerpt_length( $length ) {
     return 30;
 }
 add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
-
-function wpdocs_theme_name_scripts() {
-  wp_enqueue_style( 'style-name', get_stylesheet_uri() );
-  wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
-}
-add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
